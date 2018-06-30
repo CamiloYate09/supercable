8095650c200693355cb18f91e1790a8d56fa8107
CamiloYate09 <cyateya35439@universidadean.edu.co>
2018-06-30 01:21:28 -0500

    first commit

--- vista/administrador.php
+++ vista/administrador.php
@@ -1,53 +1,98 @@
 <?php
 
-class Users{
-//$con = pg_connect("host=ec2-54-227-237-27.compute-1.amazonaws.com port=5432  dbname=ddob655ldc9d3u  user=lhpkliyrbkfwac  password=c4261efd43b96cb28c5cd52efe6d04298f0cd9b7ab88fe269313cfd798027670");
+session_start();
 
-    protected $db;
-    protected $faker;
+//INICIALIZAR VARIABLES
 
-
-    public function __construct()
-    {
-        $this->db = pg_connect("host=ec2-54-227-237-27.compute-1.amazonaws.com port=5432  dbname=ddob655ldc9d3u  user=lhpkliyrbkfwac  password=c4261efd43b96cb28c5cd52efe6d04298f0cd9b7ab88fe269313cfd798027670");
-        echo "Error al establecer la conexion" . pg_errormessage();
-        $this->faker = Faker\Factory::create();
-        echo "Error al establecer la conexion" . pg_errormessage();
-    }
-
-    public function generaUser(){
-        $ps = $this->db->prepare("INSERT INTO ventas(contrato,nombre,apellido,tipocedula,
-cc,email,hobby1,hobby2,hobby3) VALUES (:contrato,:nombre,:apellido,:tipocedula,:cc,:email,:hobby1,:hobby2:,hobby3)");
-        echo "Error al establecer la conexion" . pg_errormessage();
-        $resultado = pg_query($ps);
-
-        $ps->execute([
-            ':contrato' => $this->faker->ean13,
-            ':nombre' => $this->faker->name,
-            ':apellido' => $this->faker->city,
-            ':tipocedula' => $this->faker->licenceCode,
-            ':cc' => $this->faker->dni,
-            ':email' => $this->faker->email,
-            ':hobby1' => $this->faker->township,
-            ':hobby2' => $this->faker->township,
-            ':hobby3' => $this->faker->township,
-        ]);
+$contrato = "";//1
+$nombre = "";//2
+$apellido = "";//3
+$tipo = "";//4
+$numerodocumento = "";//5
+$email = "";//6
+$hobby1 = "";//7
+$hobby2 = "";//8
+$hobby3 = "";//9
 
 
 
-        echo "Error al establecer la conexion" . pg_errormessage();
-    }
 
-    public function getAll(){
-        $ps = $this->db->prepare("SELECT * FROM ventas");
-        $ps->execute();
+//actualizar registros
+//3
+$edit_state = false;
 
-        return $ps->fetchAll(PDO::FETCH_OBJ);
+
+//conectar con la base de datos
+
+$db = pg_connect("host=ec2-54-227-237-27.compute-1.amazonaws.com port=5432  dbname=ddob655ldc9d3u  user=lhpkliyrbkfwac  password=c4261efd43b96cb28c5cd52efe6d04298f0cd9b7ab88fe269313cfd798027670");
+
+// si se presiona el boton save de el formulario
+
+if(isset($_POST['save'])){
+    //2)empiezo a pasar los nombres de los atributos de mi formulario
+
+    //$name = $_POST['name'];
+    //$address = $_POST['address'];
+    $contrato = $_POST['contrato'];
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $tipo = $_POST['tipo'];
+    $numerodocumento = $_POST['numerodocumento'];
+    $email = $_POST['email'];
+    $hobby1 = $_POST['hobby1'];
+    $hobby2 = $_POST['hobby2'];
+    $hobby3 = $_POST['hobby3'];
+
+
+    //3) hacemos la query de insertar datos
+
+    $query = "INSERT INTO VENTAS(contrato,nombre,apellido,tipocedula,cc,email,hobby1,hobby2,hobby3) VALUES('$contrato', '$nombre',
+        '$apellido','$tipo','$numerodocumento','$email','$hobby1','$hobby2','$hobby3')";
+    pg_query($con, $query);
+    //Mostrar notificaciones de mensajes
+    //3
+    $_SESSION['msg'] = "Imformacion Guardada";
+
+    header('location: prueba.php'); //redireccionamos a la pagina principal
+
 }
 
+//actualizar registros
+//4)-->
+
+if (isset($_POST['update'])){
+    $contrato  = pg_escape_string($_POST['contrato']);
+    $nombre = pg_escape_string($_POST['nombre']);
+    $apellido = pg_escape_string($_POST['apellido']);
+    $tipo = pg_escape_string($_POST['tipo']);
+    $numerodocumento = pg_escape_string($_POST['numerodocumento']);
+    $fecha_ingreso    = pg_escape_string($_POST['fecha_ingreso']);
+    $email = pg_escape_string($_POST['email']);
+    $hobby1 = pg_escape_string($_POST['hobby1']);
+    $hobby2 = pg_escape_string($_POST['hobby2']);
+    $hobby3 = pg_escape_string($_POST['hobby3']);
+
+
+    pg_query($db, "UPDATE ventas SET contrato='$contrato', nombre='$nombre', apellido='$apellido', tipo='$tipo' , numerodocumento='$numerodocumento', email='$email', hobby1='$hobby1', hobby2='$hobby2',hobby3='$hobby3 WHERE contrato=$contrato");
+    $_SESSION['msg'] = "Imformacion Actualizada";
+
+    header('location: prueba.php'); //redireccionamos a la pagina principal
 }
 
+//<!-- BORRAR REGISTROS
+//5)-->
+if(isset($_GET['del'])){
+    $empleado_id = $_GET['del'];
+    pg_query($db, "DELETE FROM ventas WHERE contrato=$contrato");
+    $_SESSION['msg'] = "Imformacion Eliminada";
 
+    header('location: prueba.php'); //redireccionamos a la pagina principal
+
+}
+
+//recuperar registros##################
+//6)
+$results = pg_query($db, "SELECT * FROM ventas");
 
 
 ?>
--- vista/prueba.php
+++ vista/prueba.php
@@ -1,18 +1,286 @@
 <?php
-//include_once('controlador/salir.php');
-include_once 'vendor/autoload.php';
-include_once 'vista/administrador.php';
+include_once('administrador.php');
 
-$uo = new Users();
-echo "Error al establecer la conexion" . pg_errormessage();
+//fetch the record to be updated
+if(isset($_GET['edit'])){
+    $contrato = $_GET['edit'];
+    $edit_state = true;
+    //frontend 7) llamara datos
+    $rec = pg_query($db, "SELECT * FROM ventas WHERE contrato=$contrato");
+    $record = pg_fetch_array($rec);
+    $contrato = $record['contrato'];
+    $nombre = $record['nombre'];
+    $apellido = $record['apellido'];
+    $tipo = $record['tipo'];
+    $numerodocumento = $record['numerodocumento'];
+    $email = $record['email'];
+    $hobby1 = $record['hobby1'];
+    $hobby2= $record['hobby2'];
+    $hobby3 = $record['hobby3'];
 
-//for ($i = 0; $i < 5; $i++) {
-//
-//    $uo->generaUser();
-//    echo "Error al establecer la conexion" . pg_errormessage();
-//}
 
-$uo->generaUser();
-dd($uo->getAll());
+}
 
 ?>
+
+
+<!DOCTYPE HTML>
+
+<html lang="es">
+
+<head>
+    <title>Administrador</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="/styles/semantic.min.css">
+    <!--    //SEMANTIC UI CON  JQUERY-->
+
+    <script
+        src="https://code.jquery.com/jquery-3.1.1.min.js"
+        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+        crossorigin="anonymous"></script>
+    <script src="/js/semantic.js"></script>
+
+</head>
+<body >
+<!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
+<div class="ui teal  menu">
+    <div class="header item">
+        CRUD PHP HEROKU POSTGRESQL
+    </div>
+    <a class="item active " href="index.php">
+        EMPLOYEES
+    </a>
+    <a class="item" href="DEPARTMENTS.php">
+        DEPARTMENTS
+    </a>
+    <a class="item" href="JOBS.php">
+        JOBS
+    </a>
+</div>
+<!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
+
+<div class="ui small circular rotate left reveal image imgcenter">
+    <img src="imagenes/heroku.png" class="visible content ">
+    <img src="imagenes/1200px-Postgresql_elephant.svg.png" class="hidden content">
+</div>
+<!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
+
+<!--//Mostrar notificaciones de mensajes
+//3-->
+<?php if (isset($_SESSION['msg'])): ?>
+    <div class="msg">
+        <?php
+        echo $_SESSION['msg'];
+        unset($_SESSION['msg']);
+        ?>
+    </div>
+
+<?php endif;  ?>
+
+
+
+<table class="ui inverted grey celled">
+    <thead>
+    <tr>
+        <!--fronted 8) desplegar datos en html-->
+        <th>contrato</th>
+        <th>nombre</th>
+        <th>apellido</th>
+        <th>tipo</th>
+        <th>numerodocumento</th>
+        <th>email</th>
+        <th>hobby1</th>
+        <th>hobby2</th>
+        <th>hobby3</th>
+
+
+        <th colspan="2" >Action</th>
+    </tr>
+
+    </thead>
+
+
+    <tbody>
+    <!-- 9)llamando registros de la base de datos-->
+    <?php while ($row = pg_fetch_array($results))   { ?>
+        <tr>
+            <td><?php echo $row['contrato']; ?></td>
+            <td><?php echo $row['nombre']; ?></td>
+            <td><?php echo $row['apellido']; ?></td>
+            <td><?php echo $row['tipo']; ?></td>
+            <td><?php echo $row['numerodocumento']; ?></td>
+            <td><?php echo $row['email']; ?></td>
+            <td><?php echo $row['hobby1']; ?></td>
+            <td><?php echo $row['hobby2']; ?></td>
+            <td><?php echo $row['hobby3']; ?></td>
+            <td>
+                <!--10)//actualizar registros-->
+                <a class="ui circular blue  icon button" href="/vista/prueba.php?edit=<?php echo $row['contrato'];?>"> Edit</a>
+            </td>
+            <td>
+                <!-- 11)BORRAR REGISTROS-->
+                <a class="ui circular teal twitter icon button" href="/vista/administrador.php?del=<?php echo $row['contrato'];?>" >Delete</a>
+            </td>
+        </tr>
+
+    <?php }?>
+    </tbody>
+
+</table>
+<!--server.php conexion con la base de datos-->
+<form action="/vista/administrador.php"  method="POST" class="ui form" >
+    <!--//12) actualizar registros en el formulario-->
+    <input type="hidden" name="contrato"  value="<?php echo $contrato;?>">
+
+    <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
+    <!--<div class="ui right labeled input field">
+        <input type="number" placeholder="Enter ..." autofocus maxlength="3" name="empleado_id" value="<?php /*echo $empleado_id;*/?>">
+        <div class="ui teal  label">
+            empleado_id
+        </div>
+    </div>
+-->
+
+
+
+
+    <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
+
+    <div class="field">
+        <div class="two fields">
+
+            <div class="ui right labeled input corner">
+                <input type="number" placeholder="Enter ..." autofocus maxlength="20" name="contrato" value="<?php echo $contrato;?>">
+                <div class="ui teal   label">
+                    contrato
+                </div>
+            </div>
+
+
+
+            <div class="ui right labeled input ">
+                <input type="text" placeholder="Enter ..." autofocus maxlength="20" name="nombre" value="<?php echo $nombre;?>">
+                <div class="ui teal   label">
+                    Nombre
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
+
+    <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
+
+    <div class="field">
+        <div class="two fields">
+            <div class="ui right labeled input ">
+                <input type="text" placeholder="Enter ..." name="apellido" autofocus maxlength="50" value="<?php echo $apellido;?>">
+                <div class="ui teal   label">
+                    Apellido
+                </div>
+            </div>
+
+
+
+            <div class="ui right labeled input ">
+                <input type="text" placeholder="Enter ..." autofocus maxlength="15" name="tipo" value="<?php echo $tipo;?>">
+                <div class="ui teal   label">
+                    Tipo
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
+
+    <div class="field">
+        <div class="two fields">
+            <div class="ui right labeled input ">
+                <input type="number" placeholder="Enter..." name="numerodocumento" value="<?php echo $numerodocumento;?>">
+                <div class="ui teal   label">
+                    Numero Documento
+                </div>
+            </div>
+
+
+
+            <div class="ui right labeled input ">
+                <input type="email" placeholder="Enter ..." autofocus maxlength="20" name="email" value="<?php echo $email;?>">
+                <div class="ui teal   label">
+                    Email
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
+
+    <div class="field">
+        <div class="two fields">
+            <div class="ui right labeled input ">
+                <input type="text" placeholder="Enter ..." autofocus maxlength="14" name="hobby1" value="<?php echo $hobby1;?>">
+                <div class="ui teal   label">
+                    Hobby1
+                </div>
+            </div>
+
+
+
+            <div class="two fields">
+                <div class="ui right labeled input ">
+                    <input type="text" placeholder="Enter ..." autofocus maxlength="14" name="hobby2" value="<?php echo $hobby2;?>">
+                    <div class="ui teal   label">
+                        Hobby2
+                    </div>
+                </div>
+
+            </div>
+    </div>
+
+    <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
+    <div class="field">
+        <div class="two fields">
+            <div class="ui right labeled input ">
+                <input type="text" placeholder="Enter ..." autofocus maxlength="14" name="hobby3" value="<?php echo $hobby3;?>">
+                <div class="ui teal   label">
+                    Hobby3
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+        
+
+
+    <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
+    <div class="input-group">
+        <!-- //actualizar registros
+         //3-->
+        <?php   if ($edit_state == false): ?>
+
+            <button type="submit" name="save" class="btn" onclick="alert('Tu imformacion se esta actualizando')" >Guardar</button>
+            <button type="button" onclick="alert('Tu imformacion se esta actualizando')"  class="ui secondary  loading button positive"></button>
+        <?php else: ?>
+
+            <button type="submit" name="update" class="btn" onclick="alert('Tu imformacion se esta actualizando')" >Actualizar</button>
+            <button type="button" onclick="alert('Tu imformacion se esta actualizando')"  class="ui secondary  loading button positive"></button>
+        <?php endif; ?>
+
+    </div>
+
+    <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
+
+
+
+</form>
+
+
+</body>
+
+
+</html>
