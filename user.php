b55542bcd00e35c52de80fe9b780b25011ea3309
eaquimbayo <eaquimbayo@hotmail.com>
2018-09-01 10:00:03 -0500

    cambiando aplicacion

--- user.php
+++ user.php
@@ -74,7 +74,22 @@
         <div class="col-md-9">
             <div >
                 <h1>Bienvenido a Learnet</h1>
-                <h4>Ingresar nueva Venta</h4>
+     
+
+
+     <div class="container">
+  <div class="row">
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+</div>
 
 
 
